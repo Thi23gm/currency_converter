e86f7a5d1b01cbd9bd47e7688bf3c8397ec7394c
Thiago Gomes <thi23gm@gmail.com>
2023-05-03 15:20:27 -0300

    add Shered Preferences, Ligth mode and Dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 final String apiKey = 'OZ38YS18TalFGcYxSDkS7UtXgDFEl136';
 final String apiUrl = 'https://api.apilayer.com/exchangerates_data/convert?';
@@ -30,6 +31,7 @@ Future<double> converterMoeda(
 
 void main() {
   runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: MoneyConverter(),
   ));
 }
@@ -46,6 +48,7 @@ class _MoneyConverterState extends State<MoneyConverter> {
   String? moedaPara;
   double valor = 0;
   double valorConvertido = 0;
+  bool isDarkMode = false;
 
   final List<String> moedas = [
     'USD',
@@ -56,132 +59,122 @@ class _MoneyConverterState extends State<MoneyConverter> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _loadIsDarkMode();
+  }
+
+  _loadIsDarkMode() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    });
+  }
+
+  _toggleDarkMode(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDarkMode = value;
+      prefs.setBool('isDarkMode', value);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text("Conversor de Moedas"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Conversor de Moedas"),
+          ),
         ),
-        backgroundColor:
-            Color.fromARGB(78, 14, 19, 30), // Cor de fundo da app bar
-      ),
-      body: Container(
-        color: Color.fromARGB(248, 220, 211, 211), // Cor de fundo do body
-        padding: const EdgeInsets.all(15),
-        child: Column(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            DropdownButton<String>(
-              value: moedaDe,
-              onChanged: (String? newValue) {
-                setState(() {
-                  moedaDe = newValue;
-                });
-              },
-              items: moedas.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(
-                    value,
-                    style: TextStyle(
-                        color: Colors.black), // Cor do texto do dropdown
-                  ),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 20),
-            DropdownButton<String>(
-              value: moedaPara,
-              onChanged: (String? newValue) {
-                setState(() {
-                  moedaPara = newValue;
-                });
-              },
-              items: moedas.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(
-                    value,
-                    style: TextStyle(
-                        color: Colors.black), // Cor do texto do dropdown
-                  ),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  valor = double.tryParse(value) ?? 0;
-                });
-              },
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Digite o valor a ser convertido',
-                labelStyle:
-                    TextStyle(color: Colors.black), // Cor do texto da label
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Digite um valor',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    valor = double.parse(value);
+                  });
+                },
               ),
             ),
-            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  DropdownButton<String>(
+                    value: moedaDe,
+                    hint: const Text('Moeda de origem'),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        moedaDe = newValue!;
+                      });
+                    },
+                    items: moedas.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  const Icon(Icons.arrow_forward),
+                  DropdownButton<String>(
+                    value: moedaPara,
+                    hint: const Text('Moeda de destino'),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        moedaPara = newValue!;
+                      });
+                    },
+                    items: moedas.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
             ElevatedButton(
               onPressed: () async {
-                try {
-                  if (moedaDe == null || moedaPara == null || moedaPara == "") {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Erro'),
-                          content: const Text(
-                              'Por favor, selecione as moedas de origem e destino.'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: const Text('OK'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  } else {
-                    double resultado =
-                        await converterMoeda(valor, moedaDe!, moedaPara!);
-                    setState(() {
-                      valorConvertido = resultado;
-                    });
-                  }
-                } catch (e) {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Erro'),
-                        content: Text('Falha ao converter moeda: $e'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: const Text('OK'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
+                if (moedaDe != null && moedaPara != null) {
+                  double resultado =
+                      await converterMoeda(valor, moedaDe!, moedaPara!);
+                  setState(() {
+                    valorConvertido = resultado;
+                  });
                 }
               },
-              style: ElevatedButton.styleFrom(
-                primary:
-                    Color.fromRGBO(12, 165, 176, 1.0), // Define a cor do botão
-              ),
               child: const Text('Converter'),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 32.0),
             Text(
-              'Valor convertido: ${valorConvertido.toStringAsFixed(2)} ${moedaPara != null ? moedaPara : ''}',
-              style: const TextStyle(fontSize: 18),
+              'Resultado: ${valorConvertido.toStringAsFixed(2)} $moedaPara',
+              style: const TextStyle(fontSize: 24.0),
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Modo escuro'),
+                Switch(
+                  value: isDarkMode,
+                  onChanged: _toggleDarkMode,
+                ),
+              ],
             ),
           ],
         ),
